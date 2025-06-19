9750d5482bdd1643114ee12b3b5588cce699ece5
jockopw <jockj9963@gmail.com>
2025-06-20 00:42:43 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <button onClick={() => alert("Button clicked!")}>Click Me</button>
+      <button onClick={() => alert("Button 1 clicked!")}>Button 1</button>
+      <button onClick={() => alert("Button 2 clicked!")}>Button 2</button>
+      <button onClick={() => alert("Button 3 clicked!")}>Button 3</button>
     </div>
   );
 }
